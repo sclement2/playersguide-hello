7f1d8d67f40b5a55488200f6184d454353a4aa2c
sclement2 <sclement2@gmail.com>
2024-02-11 04:59:11 +0000

    Create the Thing Namer 3000

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
-namespace dotnetcore
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+Console.WriteLine("What kind of thing are we talking about?"); 
+string thingName = CapitalizeFirstLetter(Console.ReadLine()); /* Name of the "thing" */
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?"); 
+string thingDescription = CapitalizeFirstLetter(Console.ReadLine()); // Short describer of the "thing"
+
+string thingModifier = "of Doom"; /* Modifier to add to the description */
+string thingVersion = "3000"; // Version of the "thing"
+Console.WriteLine("The " + thingDescription + thingName + thingModifier + " " + thingVersion + "!");
+
+static string CapitalizeFirstLetter(string source) {
+    if (string.IsNullOrEmpty(source))
+        return string.Empty;
+
+    char[] letters = source.ToCharArray(); // convert to char array of the string
+    letters[0] = char.ToUpper(letters[0]); // capitalize first char
+    return new string(letters) + " "; // return the array made of the new char array
 }
